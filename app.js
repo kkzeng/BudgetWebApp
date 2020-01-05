ae4c5febbcecd6b13b760bf99c5550320f11e7af
Kenneth Zeng <kkzeng@edu.uwaterloo.ca>
2020-01-05 15:30:29 +0800

    Clear input fields after adding

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// MODULE PATTERN
+
 // IIFE to encapsulate 
 // BUDGET CONTROLLER
 var budgetController = (function() {
@@ -27,15 +29,54 @@ var budgetController = (function() {
         }
     }
 
+    // Return object containing public interface
+    return {
+        addItem: function(type, desc, value) {
+            var toAdd;
+            var ID = 0;
+            // Want next ID to be last item's ID + 1
+            // Calculate ID
+            if(data.allItems[type].length > 0) ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+
+            if(type === 'inc') {
+                toAdd = new Income(ID, desc, value);
+            }
+            else {
+                toAdd = new Expense(ID, desc, value);
+            }
+
+            // Can use type to access correct array in object
+            data.allItems[type].push(toAdd);
+
+            // Update total
+            data.totals[type] += value;
+
+            // Return the new element for use in other modules
+            return toAdd;
+        },
+        // Testing function
+        getTotal: function(type) {
+            return data.totals[type];
+        }
+    }
+
 })();
 
 // UI CONTROLLER
 var UIController = (function() {
     var DOMstrs = {
+        // Form
         inputType: '.add__type',
         inputDesc: '.add__description',
-        inputVal: '.add__type',
-        addBtn: '.add__btn'
+        inputVal: '.add__value',
+        addBtn: '.add__btn',
+        // List of items
+        incList: '.income__list',
+        expList: '.expenses__list',
+        // Top summary
+        netBudget: '.budget__value',
+        incTotal: '.budget__income--value',
+        expTotal: '.budget__expenses--value'
     }
 
     return {
@@ -44,13 +85,58 @@ var UIController = (function() {
             // Get all the values
             return {
                 desc: document.querySelector(DOMstrs.inputDesc).value, // description
-                value: document.querySelector(DOMstrs.inputVal).value, // how much money
+                value: parseInt(document.querySelector(DOMstrs.inputVal).value), // how much money
                 type: document.querySelector(DOMstrs.inputType).value // plus or minus
             }
         },
         // Retrieve DOM strings for each selector
         getDOMStrs: function() {
             return DOMstrs;
+        },
+        // Add list item to UI
+        addListItem: function(object, type) {
+            // object can be Income or Expense
+
+            // Create HTML string with placeholder
+            var html = 
+            `<div class="item clearfix" id="income-%id%">
+                <div class="item__description">%desc%</div>
+                <div class="right clearfix">
+                    <div class="item__value">${type === 'exp'? '-': '+'} %value%</div>
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>`;
+            
+            // Replace placeholder with real data
+            var newHtml = html.replace('%id%', object.id);
+            newHtml = newHtml.replace('%desc%', object.desc);
+            newHtml = newHtml.replace('%value%', object.value);
+
+            // Insert HTML into DOM
+            
+            document.querySelector(DOMstrs[type+'List']).insertAdjacentHTML('afterbegin', newHtml);
+        },
+        // Update totals
+        // Private function to update totals
+        updateTotals: function(incTotal, expTotal) {
+            document.querySelector(DOMstrs.incTotal).textContent = incTotal;
+            document.querySelector(DOMstrs.expTotal).textContent = expTotal;
+            document.querySelector(DOMstrs.netBudget).textContent = incTotal - expTotal;
+        },
+        // Clear the input fields - used after adding an item
+        clearFields: function() {
+            // Select all the input fields
+            var fields = document.querySelectorAll(DOMstrs.inputDesc + ', ' + DOMstrs.inputVal);
+            // Get a copy of the fields
+            var fieldArr = Array.prototype.slice.call(fields);
+            // Clear them
+            fieldArr.forEach(function(cur, idx, arr) {
+                cur.value = '';
+            });
+            // Move keyboard focus back to the first input field
+            fieldArr[0].focus();
         }
     };
 })();
@@ -76,22 +162,37 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     };
 
+    // Private function to update summary
+    function updateSummary() {
+        var incTotal = budgetController.getTotal('inc');
+        var expTotal = budgetController.getTotal('exp')
+        UICtrl.updateTotals(incTotal, expTotal);
+    }
+
     var ctrlAddItemFunction = function() {
+        var inputs, item;
+
         // 1. Get input values
-        var inputs = UICtrl.getInput();
-        console.log(inputs);
+        inputs = UICtrl.getInput();
+        
         // 2. Add the item to budget controller
+        item = budgetCtrl.addItem(inputs.type, inputs.desc, inputs.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(item, inputs.type);
+                
+        // 6. Clear the input fields
+        UICtrl.clearFields();
 
         // 4. Calculate the new balance
+        updateSummary();
 
-        // 5. Display the budget
+        // 5. Display the budget        
     };
 
     return {
         init: function() {
-            console.log('Application has started');
+            updateSummary();
             setupEventListeners();
         }
     }
